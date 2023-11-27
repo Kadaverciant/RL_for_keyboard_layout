b9ee85aa020def25e403c7f915158af5770d18c7
Dmitry <pro100pro10010@gmail.com>
2023-11-28 01:20:49 +0300

    Add [baseline] src

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,3 +1,9 @@
+from copy import deepcopy
+from typing import Literal, Optional
+
+import numpy as np
+import torch
+
 QWERTY_LOW_LAYOUT: list[list[str]] = [
     ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "<back>"],
     ["<tab>", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\"],
@@ -130,7 +136,7 @@ def encode_decode_buttons(buttons: set[str]) -> tuple[dict[str, int], dict[int, 
     encode_value = len(letters_dict) + 1
     encode_dict = {}
     decode_dict = {}
-    for btn in buttons:
+    for btn in sorted(buttons):
         if btn in letters_dict:
             decode_dict[letters_dict[btn]] = btn
             encode_dict[btn] = letters_dict[btn]
@@ -156,3 +162,343 @@ SPECIAL_SYMBOLS = {
     "\n": "<enter>",
     " ": "<space>",
 }
+
+Layout = list[list[int]]
+
+
+def encode_layout(layout: list[list[str]]) -> Layout:
+    return [[ENCODE_DICT[btn] for btn in layout[i]] for i in range(len(layout))]
+
+
+def decode_layout(layout: Layout) -> list[list[str]]:
+    return [[DECODE_DICT[btn] for btn in layout[i]] for i in range(len(layout))]
+
+
+QWERTY_ENCODED_HIGH: Layout = encode_layout(QWERTY_HIGH_LAYOUT)
+QWERTY_ENCODED_LOW: Layout = encode_layout(QWERTY_LOW_LAYOUT)
+
+
+def get_all_buttons_encoded(high_layout: Layout, low_layout: Layout) -> list[int]:
+    all_buttons = []
+
+    for layout in (low_layout, high_layout):
+        for row in layout:
+            all_buttons.extend(row)
+    return all_buttons
+
+
+ALL_BUTTONS_ENCODED = get_all_buttons_encoded(QWERTY_ENCODED_HIGH, QWERTY_ENCODED_LOW)
+
+
+Position = tuple[int, int]
+
+
+LogType = Literal["basic"] | Literal["debug"] | Literal["error"]
+
+
+class Logger:
+    def __init__(self, verbose: bool = True, hide_types: list[LogType] = []) -> None:
+        self.verbose = verbose
+        self.hide_types = set(hide_types)
+
+    def log(self, message: str, log_type: LogType = "basic") -> None:
+        if self.verbose and log_type not in self.hide_types:
+            print(message)
+
+
+LOGGER = Logger()
+
+
+class Finger:
+    def __init__(
+        self, initial_position: Position, name: str, logger: Logger = LOGGER
+    ) -> None:
+        self.name = name
+        self.initial_position = initial_position
+
+        self.logger = logger
+
+        self.reset()
+
+        # Constants
+
+        self.wait_before_return = 4  # in ticks
+
+        self.long_row_move_shift = 3
+        self.long_row_move_penalty = 1
+
+        self.row_penalty_coefficient = 1
+        self.column_penalty_coefficient = 1.2
+
+    def reset(self):
+        self.current_position = self.initial_position
+        self.ticks_before_return = 0  # if == 0, returns to the initial position
+        self.typed_keys = 0
+
+    def move(self, position: Position):
+        self.current_position = position
+
+        self.ticks_before_return = self.wait_before_return
+        self.typed_keys += 1
+
+    def tick(self) -> float:
+        if self.ticks_before_return > 0:
+            self.ticks_before_return -= 1
+
+        if self.ticks_before_return == 0:
+            score = self.get_score(self.initial_position)
+            self.current_position = self.initial_position
+            return score
+
+        return 0
+
+    def get_score(self, target_position: Position) -> float:
+        x1, y1 = self.current_position
+        x2, y2 = target_position
+
+        row_distance = abs(x1 - x2) ** 2
+        column_distance = abs(y1 - y2) ** 2
+
+        penalty = 0
+        if row_distance > self.long_row_move_shift:
+            penalty = self.long_row_move_penalty
+        return (
+            row_distance * self.row_penalty_coefficient
+            + column_distance * self.column_penalty_coefficient
+            + penalty
+        )
+
+    def show_statistics(self):
+        self.logger.log(
+            f"Name: {self.name:22} \
+            Typed keys: {self.typed_keys:5} \
+            Ticks before return: {self.ticks_before_return:5} \
+            Current position: {self.current_position}\t\
+            Default position: {self.initial_position}"
+        )
+
+
+DEFAULT_FINGERS: list[Finger] = [
+    Finger((2, 1), "левый мизинец"),
+    Finger((2, 2), "левый безымянный"),
+    Finger((2, 3), "левый средний"),
+    Finger((2, 4), "левый указательный"),
+    Finger((4, 3), "левый большой"),
+    Finger((4, 6), "правый большой"),
+    Finger((2, 7), "правый указательный"),
+    Finger((2, 8), "правый средний"),
+    Finger((2, 9), "правый безымянный"),
+    Finger((2, 10), "правый мизинец"),
+]
+
+SwapType = Literal["low_layout"] | Literal["high_layout"] | Literal["between_layouts"]
+
+
+class KeyboardLayout:
+    @staticmethod
+    def layout_to_dict(
+        layout: Layout, unused_layout: Layout
+    ) -> dict[int, list[Position]]:
+        layout_dict: dict[int, list[Position]] = {}
+
+        for i in range(len(layout)):
+            for j in range(len(layout[i])):
+                button = layout[i][j]
+                if button in layout_dict:
+                    layout_dict[button].append((i, j))
+                else:
+                    layout_dict[button] = [(i, j)]
+
+        for i in range(len(unused_layout)):
+            for j in range(len(unused_layout[i])):
+                button = unused_layout[i][j]
+                if button not in layout_dict:
+                    layout_dict[button] = []
+
+        return layout_dict
+
+    def _finish_move(self):
+        for finger in self.fingers:
+            self.total_score += finger.tick()
+
+    def __init__(self, low_layout: Layout, high_layout: Layout, logger: Logger = LOGGER):
+        self.low_layout = deepcopy(low_layout)
+        self.high_layout = deepcopy(high_layout)
+
+        self.low_layout_dict = KeyboardLayout.layout_to_dict(
+            self.low_layout, self.high_layout
+        )
+        self.high_layout_dict = KeyboardLayout.layout_to_dict(
+            self.high_layout, self.low_layout
+        )
+
+        self.logger = logger
+
+        self.fingers = deepcopy(DEFAULT_FINGERS)
+
+        self.reset()
+
+    def reset(self):
+        self.total_score: float = 0
+        self.typed_keys: int = 0
+        for f in self.fingers:
+            f.reset()
+
+    def move_one_finger(
+        self, positions: list[Position], busy_finger_id: Optional[int] = None
+    ) -> tuple[tuple[int, Position], float]:
+        best_finger_id: int = 0
+        best_score = np.inf
+
+        final_position: Position = (0, 0)
+
+        for position in positions:
+            scores = [
+                finger.get_score(position) if i != busy_finger_id else np.inf
+                for i, finger in enumerate(self.fingers)
+            ]
+
+            candidate_finger_id = int(np.argmin(scores))
+            candidate_score = scores[candidate_finger_id]
+
+            if candidate_score < best_score:
+                best_score = candidate_score
+                best_finger_id = candidate_finger_id
+                final_position = position
+
+        return (best_finger_id, final_position), best_score
+
+    def move_two_fingers(
+        self, positions: list[Position]
+    ) -> tuple[tuple[int, Position], tuple[int, Position], float]:
+        shift_positions = self.low_layout_dict[SHIFT_CODE]
+        if len(shift_positions) == 0:
+            print("ERROR SHIFT IS UNREACHABLE")
+            return (0, (0, 0)), (0, (0, 0)), 9999
+
+        # firstly reach SHIFT, then - positions
+        finger_shift_info_1, shift_distance_1 = self.move_one_finger(shift_positions)
+        finger_btn_info_1, d1_btn = self.move_one_finger(
+            positions, finger_shift_info_1[0]
+        )
+        total_distance_1 = shift_distance_1 + d1_btn
+
+        # firstly reach positions, then - SHIFT
+        finger_btn_info_2, d1_btn = self.move_one_finger(positions)
+        finger_shift_info_2, shift_distance_2 = self.move_one_finger(
+            shift_positions, finger_btn_info_2[0]
+        )
+        total_distance_2 = shift_distance_2 + d1_btn
+
+        if total_distance_1 < total_distance_2:
+            return finger_btn_info_1, finger_shift_info_1, total_distance_1
+
+        return finger_btn_info_2, finger_shift_info_2, total_distance_2
+
+    def find_button(self, button: int):
+        if button in self.low_layout_dict and len(self.low_layout_dict[button]) > 0:
+            (finger_id, finger_position), score = self.move_one_finger(
+                self.low_layout_dict[button]
+            )
+
+            self.fingers[finger_id].move(finger_position)
+            self.total_score += score
+            self.typed_keys += 1
+
+            self.logger.log(f"{button}:\t{self.fingers[finger_id].name}")
+
+        elif button in self.high_layout_dict and len(self.high_layout_dict[button]) > 0:
+            (
+                (finger_id_1, finger_position_1),
+                (finger_id_2, finger_position_2),
+                score,
+            ) = self.move_two_fingers(self.high_layout_dict[button])
+
+            self.fingers[finger_id_1].move(finger_position_1)
+            self.fingers[finger_id_2].move(finger_position_2)
+            self.total_score += score
+            self.typed_keys += 2
+
+            self.logger.log(
+                f"{button}:\t{self.fingers[finger_id_1].name} + {self.fingers[finger_id_2].name}"
+            )
+
+        else:
+            self.logger.log(f"NO SUCH KEY: {button}")
+
+        self._finish_move()
+
+    def type_text(self, text: list[str]) -> float:
+        for button in text:
+            self.find_button(ENCODE_DICT[button])
+        return self.total_score
+
+    def type_encoded_text(self, encoded_text: list[int]) -> float:
+        for button in encoded_text:
+            self.find_button(button)
+        return self.total_score
+
+    def swap_buttons(self, position1: Position, position2: Position, swap_type: SwapType):
+        if swap_type == "high_layout":
+            layout_from = layout_to = self.high_layout
+            layout_from_dict = layout_to_dict = self.high_layout_dict
+        elif swap_type == "low_layout":
+            layout_from = layout_to = self.low_layout
+            layout_from_dict = layout_to_dict = self.low_layout_dict
+        else:  # swap_type == "between_layouts"
+            layout_from = self.low_layout
+            layout_to = self.high_layout
+            layout_from_dict = self.low_layout_dict
+            layout_to_dict = self.high_layout_dict
+
+        x1, y1 = position1
+        btn1 = layout_from[x1][y1]
+        x2, y2 = position2
+        btn2 = layout_to[x2][y2]
+
+        layout_from[x1][y1], layout_to[x2][y2] = layout_to[x2][y2], layout_from[x1][y1]
+
+        layout_from_dict[btn1].remove(position1)
+        layout_to_dict[btn2].remove(position2)
+
+        layout_from_dict[btn2].append(position1)
+        layout_to_dict[btn1].append(position2)
+
+    def decode_layouts(self) -> tuple[list[list[str]], list[list[str]]]:
+        return (decode_layout(self.low_layout), decode_layout(self.high_layout))
+
+    def get_string_layouts(self) -> str:
+        low_layout, high_layout = self.decode_layouts()
+        result_string = "High layout:\n"
+        for row in high_layout:
+            for s in row:
+                result_string += f"{s:8}"
+            result_string += "\n"
+        result_string += "\n"
+
+        result_string += "\nLow layout:\n"
+        for row in low_layout:
+            for s in row:
+                result_string += f"{s:8}"
+            result_string += "\n"
+        result_string += "\n"
+
+        return result_string
+
+    def show_statistics(self):
+        self.logger.log("\nStatistics:")
+        for f in self.fingers:
+            f.show_statistics()
+
+    def flatten(self):
+        flatten = []
+        for row in self.low_layout:
+            flatten.extend(row)
+        for row in self.high_layout:
+            flatten.extend(row)
+
+        flatten = [x / len(BUTTONS_SET) for x in flatten]
+        return torch.as_tensor(flatten, dtype=torch.float32)
+
+    def get_average_score(self) -> float:
+        return self.total_score / self.typed_keys
---
+++ src/models/baseline.py
@@ -0,0 +1,255 @@
+""" Module contains functions for obtaining dataset """
+
+import argparse
+import os
+import random
+import sys
+from ast import literal_eval
+from typing import Optional
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import torch
+from tqdm import tqdm
+
+sys.path.append("./src")  # workaround to import files from ./src folders
+
+
+from keyboard import (
+    ALL_BUTTONS_ENCODED,
+    KEYBOARD_LAYOUT_SHAPE,
+    QWERTY_ENCODED_HIGH,
+    QWERTY_ENCODED_LOW,
+    SHIFT_CODE,
+    KeyboardLayout,
+    Layout,
+)
+
+# Paths from root directory
+FIGURES_SAVE_PATH = os.path.join(".", "data/figures")
+RAW_DATA_PATH = os.path.join(".", "data/raw/encoded_programs.csv")
+
+
+class Logger:
+    """Manage log messages"""
+
+    def __init__(self, verbose: bool) -> None:
+        self.verbose = verbose
+
+    def log(self, message: str):
+        """Log message to console
+
+        Args:
+            message (str): message to log
+        """
+        if self.verbose:
+            print(message)
+
+
+""" Collect data functions """
+
+
+def read_data(path: str, limit: int, logger: Logger):
+    logger.log(f"Reading data '{path}'...")
+    data_points = pd.read_csv(path, index_col=0)
+
+    # Remove all data from folder
+    dataset = np.array(
+        [
+            np.array(text)
+            for text in data_points["encoded_text"].apply(literal_eval).to_list()
+        ]
+    )
+
+    logger.log("Success!\n")
+
+    return dataset[:limit]
+
+
+""" Layout generation functions """
+
+
+def generate_random_layout(
+    all_buttons_encoded: list[int],
+    keyboard_shape: tuple[int, ...],
+    seed: Optional[int] = None,
+) -> tuple[Layout, Layout]:
+    if seed is not None:
+        random.seed(seed)
+
+    all_buttons = all_buttons_encoded.copy()
+
+    low_layout = [[0 for _ in range(row_length)] for row_length in keyboard_shape]
+    high_layout = [[0 for _ in range(row_length)] for row_length in keyboard_shape]
+
+    # Push single SHIFT to the low layout
+    all_buttons.remove(SHIFT_CODE)
+    random.shuffle(all_buttons)
+
+    shift_position_low_layout = random.randint(0, len(all_buttons_encoded) // 2)
+    all_buttons.insert(shift_position_low_layout, SHIFT_CODE)
+    pointer = 0
+    for layout in (low_layout, high_layout):
+        for i, row_length in enumerate(keyboard_shape):
+            for j in range(row_length):
+                layout[i][j] = all_buttons[pointer]
+                pointer += 1
+
+    return low_layout, high_layout
+
+
+def generate_layouts(
+    layouts_number: int,
+    logger: Logger,
+) -> list[KeyboardLayout]:
+    layouts = []
+    loop = range(layouts_number)
+    if logger.verbose:
+        loop = tqdm(loop)
+        loop.set_description(desc="Generating layouts")
+
+    for i in loop:
+        random_low, random_high = generate_random_layout(
+            ALL_BUTTONS_ENCODED, KEYBOARD_LAYOUT_SHAPE, seed=i
+        )
+        layouts.append(KeyboardLayout(random_low, random_high, Logger(verbose=False)))
+    logger.log("Success!\n")
+    return layouts
+
+
+def estimate_layouts(
+    layouts: list[KeyboardLayout], dataset: np.ndarray, logger: Logger
+) -> torch.Tensor:
+    loop = dataset
+    if logger.verbose:
+        loop = tqdm(loop)
+        loop.set_description(desc="Estimating layouts")
+    for layout in layouts:
+        layout.reset()
+    for text in loop:
+        for layout in layouts:
+            layout.type_encoded_text(text)
+    logger.log("Success!\n")
+    return torch.as_tensor([layout.total_score for layout in layouts])
+
+
+def save_scores_plot(
+    scores: torch.Tensor,
+    qwerty_score: float,
+    save_path: str,
+    logger: Logger,
+    figsize: tuple[int, int] = (16, 9),
+):
+    logger.log("Saving figure...")
+
+    qwerty_scores = [qwerty_score for _ in range(len(scores))]
+    random_scores = [x.item() for x in scores]
+    plt.subplots(1, 1, figsize=figsize)
+    points = list(range(len(random_scores)))
+    plt.scatter(points, random_scores, s=10, label="Layout score")
+    plt.plot(qwerty_scores, label="QWERTY score", color="red")
+    plt.legend()
+    plt.grid()
+    plt.ylabel("Score")
+    plt.xlabel("Random layout #")
+    plt.title("Scores for random layouts")
+    plt.savefig(save_path)
+    plt.close()
+    logger.log("Success!\n")
+
+
+""" Main function """
+
+
+def compute_baseline():
+    """Compute baseline"""
+
+    # Parse arguments
+    parser = argparse.ArgumentParser(description="Make dataset script")
+    parser.add_argument(
+        "-s",
+        "--save-path",
+        type=str,
+        dest="save_path",
+        default=FIGURES_SAVE_PATH,
+        help="path to save figures",
+    )
+    parser.add_argument(
+        "-d",
+        "--data-path",
+        type=str,
+        dest="data_path",
+        default=RAW_DATA_PATH,
+        help="path to load the raw data",
+    )
+    parser.add_argument(
+        "-l",
+        "--data-limit",
+        type=int,
+        dest="data_limit",
+        default=10,
+        help="number of used data points",
+    )
+    parser.add_argument(
+        "-n",
+        "--number-layouts",
+        type=int,
+        dest="layouts_number",
+        default=5000,
+        help="number of random layouts",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        default=True,
+        action=argparse.BooleanOptionalAction,
+        help="print information (default: True)",
+    )
+
+    namespace = parser.parse_args()
+    save_path, data_path, layouts_number, data_limit, verbose = (
+        namespace.save_path,
+        namespace.data_path,
+        namespace.layouts_number,
+        namespace.data_limit,
+        namespace.verbose,
+    )
+    verbose: bool = bool(verbose)
+
+    # Set up logger
+    logger = Logger(verbose)
+
+    # Read data from disc
+    data = read_data(data_path, data_limit, logger)
+
+    # Generate random layouts
+    logger.log("Estimate random layouts")
+    layouts = generate_layouts(
+        layouts_number,
+        logger,
+    )
+
+    # Estimate qwerty
+    logger.log("Estimate QWERTY layout")
+    qwerty_keyboard = KeyboardLayout(
+        QWERTY_ENCODED_LOW, QWERTY_ENCODED_HIGH, Logger(verbose=False)
+    )
+    qwerty_score = estimate_layouts([qwerty_keyboard], data, logger)[0].item()
+
+    # Estimate random layouts
+    layouts_scores = estimate_layouts(layouts, data, logger)
+
+    # Save figures
+    save_scores_plot(
+        layouts_scores,
+        qwerty_score,
+        os.path.join(save_path, f"baseline_{layouts_number}.png"),
+        logger,
+    )
+
+    logger.log("Done!")
+
+
+if __name__ == "__main__":
+    compute_baseline()
